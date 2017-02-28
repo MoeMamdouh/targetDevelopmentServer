37b4813ecbd60b273f11fb876aa1f84e63c3a779
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-02-28 22:03:41 +0200

    using ui router library in routes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
+// 'use strict';
+
+// var commonDir = 'common';
+// var directivesDir = commonDir + '/directives';
+// var filtersDir = commonDir + '/filters';
+// var servicesDir = commonDir + '/services';
+// var target = angular.module('target', [
+// 	'ngResource',
+// 	'ngRoute',
+// ])
+// .config(function ($routeProvider) {
+// $routeProvider
+// 	.when('/', {
+// 		templateUrl: 'modules/main/main.html',
+// 		controller: 'mainCtrl',
+// 	})
+// 	.otherwise({
+// 		redirectTo: '/test'
+// 	});
+// });
+
 'use strict';
 
 var commonDir = 'common';
@@ -6,15 +27,19 @@ var filtersDir = commonDir + '/filters';
 var servicesDir = commonDir + '/services';
 var target = angular.module('target', [
 	'ngResource',
-	'ngRoute',
+	'ui.router',
 ])
-.config(function ($routeProvider) {
-$routeProvider
-	.when('/test', {
-	templateUrl: 'modules/main/main.html',
-	controller: 'mainCtrl',
+.config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
+	$stateProvider
+	.state('/', {
+		url: '/',
+		templateUrl: 'modules/main/main.html',
+		controller: 'mainCtrl',
 	})
-	.otherwise({
-	redirectTo: '/test'
-	});
+	.state('/home', {
+		templateUrl: 'modules/main/main.html',
+		controller: 'mainCtrl',
+	})
+
 });
