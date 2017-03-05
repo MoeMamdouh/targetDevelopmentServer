f48d7d3f6ba975b3a9c827fbee1dd6977da30e73
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-03-05 22:38:21 +0200

    make global data in rootscope

--- app/modules/footer/footerCtrl.js
+++ app/modules/footer/footerCtrl.js
@@ -4,9 +4,6 @@
 	footerCtrl.$injector = ['$scope'];
 
 	function footerCtrl($scope) {
-		$scope.appYear	 = config.appYear;
-		$scope.appName 	= config.appName;
-		$scope.Do 		= Do.Do;
-		$scope.DoUrl 	= Do.DoUrl;
+		
 	}
 })();
