606220057daa3c430141ed414f82980c8cadfe8e
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-03-06 20:41:28 +0200

    add services object

--- app/modules/home/homeCtrl.js
+++ app/modules/home/homeCtrl.js
@@ -4,5 +4,31 @@
 	homeCtrl.$injector = ['$scope'];
 
 	function homeCtrl($scope) {
+		$scope.services = [
+			{
+				id:1,
+				icon: 'fa fa-thumbs-up',
+				title: 'Praesent tempor purus',
+				description: 'Donec non nibh in dui sagittis finibus. Duis suscipit, arcu vel rhoncus molestie, ipsum velit mattis nibh, id congue lacus lorem a elit.',
+			},
+			{
+				id:2,
+				icon: 'fa fa-bolt',
+				title: 'Service 2',
+				description: 'Donec non nibh in dui sagittis finibus. Duis suscipit, arcu vel rhoncus molestie, ipsum velit mattis nibh, id congue lacus lorem a elit.',
+			},
+			{
+				id:1,
+				icon: 'fa fa-cog',
+				title: 'Service 3',
+				description: 'Donec non nibh in dui sagittis finibus. Duis suscipit, arcu vel rhoncus molestie, ipsum velit mattis nibh, id congue lacus lorem a elit.',
+			},
+			{
+				id:1,
+				icon: 'fa fa-hand-peace-o',
+				title: 'Service 4',
+				description: 'Donec non nibh in dui sagittis finibus. Duis suscipit, arcu vel rhoncus molestie, ipsum velit mattis nibh, id congue lacus lorem a elit.',
+			},
+		]
 	}
 })();
