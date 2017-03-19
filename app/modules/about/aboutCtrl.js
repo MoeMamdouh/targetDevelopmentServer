042bad0b2576f193e286a855c2e44ccf2be8de6c
Ahmed Samir <Ahmed Samir>
2017-03-19 22:38:58 +0200

    The About Update

--- app/modules/about/aboutCtrl.js
+++ app/modules/about/aboutCtrl.js
@@ -4,6 +4,28 @@
 	aboutCtrl.$injector = ['$scope'];
 
 	function aboutCtrl($scope) {
-      	console.log('about Ctrl')
+      	console.log('Yes We Can :D')
+	$scope.services = [
+			{
+				id:1,
+				icon: 'fa fa-user',
+				title: 'Suspendisse ornare vitae ex nec aliquam',
+				description: 'Nunc et massa ut purus porta euismod quis eu erat. Nam ornare faucibus elit sed tempor. Quisque iaculis odio nibh, et auctor tellus rhoncus vel. Maecenas arcu neque, semper eu commodo ut, pharetra vitae erat.',
+			},
+			{
+				id:2,
+				icon: 'fa fa-cogs',
+				title: 'Suspendisse ornare vitae ex nec aliquam',
+				description: 'Nunc et massa ut purus porta euismod quis eu erat. Nam ornare faucibus elit sed tempor. Quisque iaculis odio nibh, et auctor tellus rhoncus vel. Maecenas arcu neque, semper eu commodo ut, pharetra vitae erat.',
+			},
+			{
+				id:3,
+				icon: 'fa fa-thumbs-o-up',
+				title: 'Suspendisse ornare vitae ex nec aliquam',
+				description: 'Nunc et massa ut purus porta euismod quis eu erat. Nam ornare faucibus elit sed tempor. Quisque iaculis odio nibh, et auctor tellus rhoncus vel. Maecenas arcu neque, semper eu commodo ut, pharetra vitae erat.',
+			},
+
+		]
+		console.log('The End Brac')
 	}
 })();
