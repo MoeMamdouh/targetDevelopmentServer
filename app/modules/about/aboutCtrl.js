1cc2a0bf17d7b5f91e627576264b4f82d2e275be
Ahmed Samir <Ahmed Samir>
2017-03-21 23:57:22 +0200

    About

--- app/modules/about/aboutCtrl.js
+++ app/modules/about/aboutCtrl.js
@@ -25,7 +25,54 @@
 				description: 'Nunc et massa ut purus porta euismod quis eu erat. Nam ornare faucibus elit sed tempor. Quisque iaculis odio nibh, et auctor tellus rhoncus vel. Maecenas arcu neque, semper eu commodo ut, pharetra vitae erat.',
 			},
 
-		]
+		];
+	$scope.images = [
+			{
+				id:1,
+				title:'Curabitur non blandit justo',
+				url: 'images/r1.jpg',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:2,
+				title:'Curabitur non blandit justo',
+				url: 'images/r2.jpg',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:3,
+				title:'Curabitur non blandit justo',
+				url: 'images/r3.jpg',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+		];
+		$scope.Teames = [
+			{
+				id:1,
+				icon1: 'fa fa-facebook',
+				icon2: 'fa fa-twitter',
+				icon3: 'fa fa-google-plus',
+				title: 'Shane Smith',
+                url: 'images/t2.jpg',			
+			},
+			{
+				id:2,
+				icon1: 'fa fa-facebook',
+				icon2: 'fa fa-twitter',
+				icon3: 'fa fa-google-plus',
+				title: 'Shane Smith',
+				url: 'images/t3.jpg',			
+			},			
+
+			{
+				id:3,
+				icon1: 'fa fa-facebook',
+				icon2: 'fa fa-twitter',
+				icon3: 'fa fa-google-plus',
+				title: 'Suspendisse ornare vitae ex nec aliquam',
+				url: 'images/t4.jpg',			
+			},
+		];
 		console.log('The End Brac')
 	}
 })();
