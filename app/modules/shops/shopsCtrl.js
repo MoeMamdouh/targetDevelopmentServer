729dad6fbee5c78eceebef65b898c15655db555b
AHMAD\lenovo <a7mad__samir@hotmail.com>
2017-03-23 02:19:53 +0200

    Ubdate shpoes

--- app/modules/shops/shopsCtrl.js
+++ app/modules/shops/shopsCtrl.js
@@ -5,5 +5,25 @@
 
 	function shopsCtrl($scope) {
       	console.log('about Ctrl')
+	$scope.stores = [
+			{
+				id:1,
+				title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo ullamcorper aliquet. Proin est orci, suscipit aliquet cursus ut, iaculis a metus.',
+				url: 'images/tr1.jpg',
+				desc:'Aliquam dictum dui vitae leo dignissim volutpat. Etiam ultrices eget ex ut malesuada. Donec imperdiet sem quis neque dignissim, et congue felis ullamcorper. Donec vel accumsan odio. Quisque ut tortor aliquet est rutrum laoreet. Nullam sit amet lectus et leo efficitur auctor vitae sit amet metus. Aliquam pharetra gravida placerat. Sed mollis tortor tincidunt elit cursus convallis. Mauris in lacinia lectus. Duis vehicula varius quam, et rutrum turpis semper quis. Nullam rhoncus enim dui, at condimentum lectus commodo eu.',
+			},
+			{
+				id:1,
+				title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo ullamcorper aliquet. Proin est orci, suscipit aliquet cursus ut, iaculis a metus.',
+				url: 'images/tr1.jpg',
+				desc:'Aliquam dictum dui vitae leo dignissim volutpat. Etiam ultrices eget ex ut malesuada. Donec imperdiet sem quis neque dignissim, et congue felis ullamcorper. Donec vel accumsan odio. Quisque ut tortor aliquet est rutrum laoreet. Nullam sit amet lectus et leo efficitur auctor vitae sit amet metus. Aliquam pharetra gravida placerat. Sed mollis tortor tincidunt elit cursus convallis. Mauris in lacinia lectus. Duis vehicula varius quam, et rutrum turpis semper quis. Nullam rhoncus enim dui, at condimentum lectus commodo eu.',
+			},
+			{
+				id:1,
+				title:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo ullamcorper aliquet. Proin est orci, suscipit aliquet cursus ut, iaculis a metus.',
+				url: 'images/tr1.jpg',
+				desc:'Aliquam dictum dui vitae leo dignissim volutpat. Etiam ultrices eget ex ut malesuada. Donec imperdiet sem quis neque dignissim, et congue felis ullamcorper. Donec vel accumsan odio. Quisque ut tortor aliquet est rutrum laoreet. Nullam sit amet lectus et leo efficitur auctor vitae sit amet metus. Aliquam pharetra gravida placerat. Sed mollis tortor tincidunt elit cursus convallis. Mauris in lacinia lectus. Duis vehicula varius quam, et rutrum turpis semper quis. Nullam rhoncus enim dui, at condimentum lectus commodo eu.',
+			},
+		];	  
 	}
 })();
