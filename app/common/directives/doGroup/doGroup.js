5b13ae063c6c01e0eb8753baea846740f5bfd2f1
Do7a <mohamed.mumdoh@yahoo.com>
2017-03-25 02:42:21 +0200

    News View

--- app/common/directives/doGroup/doGroup.js
+++ app/common/directives/doGroup/doGroup.js
@@ -13,11 +13,21 @@
 			},
 			link: function ($scope, iElement, iAttrs) {
 				$scope.groups = _.groupBy( $scope.data, 'group');
+
 				// console.log('groups ', $scope.groups, $scope.type)
 				$scope.openLightboxModal = function (groub, index) {
 					console.log('groub ', groub)
 					Lightbox.openModal(groub, index);
 				};
+
+				$scope.split = function(array, checnkLength) {
+					console.log(' split_news ', array)
+					var split_news = _.groupBy(array, function(element, index){
+						return Math.floor(index/checnkLength);
+					}); 
+					console.log(' split_news ', split_news)
+					return split_news;
+				}
 			}
 		}
 	}
--- app/modules/news/newsCtrl.js
+++ app/modules/news/newsCtrl.js
@@ -4,6 +4,77 @@
 	newsCtrl.$injector = ['$scope'];
 
 	function newsCtrl($scope) {
-      	console.log('about Ctrl')
+	  	$scope.news = [
+			{
+				id:1,
+				title:'Curabitur non blandit justo',
+				url: 'images/r1.jpg',
+				group: 'academy',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:2,
+				title:'Curabitur non blandit justo',
+				url: 'images/r2.jpg',
+				group: 'academy',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:3,
+				title:'Curabitur non blandit justo',
+				url: 'images/r3.jpg',
+				group: 'academy',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:1,
+				title:'Curabitur non blandit justo',
+				url: 'images/r1.jpg',
+				group: 'academy',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:2,
+				title:'Curabitur non blandit justo',
+				url: 'images/r2.jpg',
+				group: 'academy',
+				group: 'academy',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:3,
+				title:'Curabitur non blandit justo',
+				url: 'images/r3.jpg',
+				group: 'academy',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+				{
+				id:1,
+				title:'Curabitur non blandit justo',
+				url: 'images/r1.jpg',
+				group: 'union',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:2,
+				title:'Curabitur non blandit justo',
+				url: 'images/r2.jpg',
+				group: 'union',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+			{
+				id:3,
+				title:'Curabitur non blandit justo',
+				url: 'images/r3.jpg',
+				group: 'union',
+				desc:'Pellentesque auctor euismod lectus a pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.nascetur ridiculus mus. Curabitur ut elit at est tempus volutpat.',
+			},
+		];
+
+		// var checnkLength = 3
+		// $scope.split_news = _.groupBy(images, function(element, index){
+		// 	return Math.floor(index/checnkLength);
+		// }); 
+		// console.log('$scope.split_news ', $scope.split_news)
 	}
 })();
